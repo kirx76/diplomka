909a5b7cc94611c5d6949c5047d33a18653d87da
kirx76 <ekirichenk8@gmail.com>
2020-02-16 23:38:16 +0300

    game started

---
+++ host.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import os
+
+import tornado.ioloop
+import tornado.web
+
+settings = {
+    "cookie_secret": "61oETzKXQAGaYdk333EmGeJfdfdh12345ddsa2JFuYh7EQnp2XdTP1o/Vo=",
+    "login_url": "/login",
+}
+
+
+# class BaseHandler(tornado.web.RequestHandler):
+#     def get_current_user(self):
+#         connect = connections.getConnection()
+#         cursor = connect.cursor()
+#         cursor.execute("select * from users")
+#         users = cursor.fetchall()
+#         connect.commit()
+#         cursor.close()
+#         connect.close()
+#         if self.get_secure_cookie("login") is not None:
+#             cookier = self.get_secure_cookie("login").decode('utf-8')
+#             for user in users:
+#                 if user['login'] == cookier:
+#                     return self.get_secure_cookie("login")
+#         return None
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('HELLO WORLD!')
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        # (r"/post/([0-9]+)", ViewPostHandler),
+    ], **settings,
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"))
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(80)
+    tornado.ioloop.IOLoop.current().start()
