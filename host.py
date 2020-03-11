b69d44302fb364b1cdf5958362f14db20daed595
kirx76 <ekirichenk8@gmail.com>
2020-03-12 00:48:48 +0300

    added user basket (demo)

--- host.py
+++ host.py
@@ -12,8 +12,10 @@ settings = {
     "login_url": "/",
 }
 
+
 def timed():
     return datetime.datetime.today().strftime("%Y")
+
 
 def get_user_info(self):
     if self.current_user is not None:
@@ -29,6 +31,7 @@ def get_user_info(self):
         return user_info
     else:
         return None
+
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
@@ -51,8 +54,12 @@ class MainHandler(BaseHandler):
     def get(self):
         connect = connections.getConnection()
         cursor = connect.cursor()
-        cursor.execute("select * from dishes")
+        cursor.execute("SELECT * FROM dishes \
+INNER JOIN dish_types \
+ON dishes.dish_type = dish_types.id ")
         dishes = cursor.fetchall()
+        cursor.execute(f"SELECT * FROM dish_types")
+        dish_types = cursor.fetchall()
         try:
             get_us = tornado.escape.xhtml_escape(self.current_user)
         except Exception as e:
@@ -60,32 +67,42 @@ class MainHandler(BaseHandler):
         if get_us != False:
             cursor.execute(f"select * from users where phone={get_us}")
             userinfo = int(cursor.fetchone()['id'])
-            cursor.execute(f"SELECT dishes.id, dishes.name, dishes.price, dishes.description, dishes.img_src, dishes.dish_type FROM users \
-    INNER JOIN users_orders \
-    ON users_orders.user_id = users.id \
-    JOIN dishes_order \
-    ON dishes_order.order_id = users_orders.order_id \
-    JOIN dishes \
-    ON dishes.id = dishes_order.dish_id \
-    WHERE users.id = {userinfo} AND users.last_order_id = dishes_order.order_id ORDER BY dishes_order.order_id desc")
+            cursor.execute(f"SELECT dishes.id, dishes.name, dishes.price, dishes.description, dishes.img_src, dish_types.id, dishes.dish_type, dish_types.dish_type_name  FROM users   \
+            INNER JOIN users_orders \
+            ON users_orders.user_id = users.id \
+            JOIN dishes_order \
+            ON dishes_order.order_id = users_orders.order_id \
+            JOIN dishes \
+            ON dishes.id = dishes_order.dish_id \
+            JOIN dish_types \
+            ON dish_types.id = dishes.dish_type \
+            WHERE users.id = {userinfo} AND users.last_order_id = dishes_order.order_id ORDER BY dishes_order.order_id desc")
+            # cursor.execute(f"SELECT * FROM dish_types")
             dishes_list = cursor.fetchall()
             cursor.close()
             connect.close()
-            self.render("index.html", dishes=dishes, timed=timed(), user=get_user_info(self), dishes_list=dishes_list)
+            self.render("index.html", dishes=dishes, timed=timed(), user=get_user_info(self), dishes_list=dishes_list,
+                        dish_types=dish_types)
         else:
-            self.render("index.html", dishes=dishes, timed=timed(), user=get_user_info(self), dishes_list=None)
+            self.render("index.html", dishes=dishes, timed=timed(), user=get_user_info(self), dishes_list=None,
+                        dish_types=dish_types)
+
 
 class viewDishInfoHandler(BaseHandler):
     def get(self, args):
         connect = connections.getConnection()
         cursor = connect.cursor()
-        cursor.execute(f"SELECT * FROM `dishes` INNER JOIN `dishes_ingredients` ON `dishes_ingredients`.`dish_id` = `dishes`.`id` LEFT JOIN `ingredients` ON `dishes_ingredients`.`ingredient_id` = `ingredients`.`id` WHERE `dishes`.`id` = {args}")
+        cursor.execute(
+            f"SELECT * FROM `dishes` INNER JOIN `dishes_ingredients` ON `dishes_ingredients`.`dish_id` = `dishes`.`id` LEFT JOIN `ingredients` ON `dishes_ingredients`.`ingredient_id` = `ingredients`.`id` WHERE `dishes`.`id` = {args}")
         dishes = cursor.fetchone()
-        cursor.execute(f"SELECT ingredients.name, ingredients.description, ingredients.calories, ingredients.id FROM `dishes` INNER JOIN `dishes_ingredients` ON `dishes_ingredients`.`dish_id` = `dishes`.`id` LEFT JOIN `ingredients` ON `dishes_ingredients`.`ingredient_id` = `ingredients`.`id` WHERE `dishes`.`id` = {args}")
+        cursor.execute(
+            f"SELECT ingredients.name, ingredients.description, ingredients.calories, ingredients.id, dishes_ingredients.weight FROM `dishes` INNER JOIN `dishes_ingredients` ON `dishes_ingredients`.`dish_id` = `dishes`.`id` LEFT JOIN `ingredients` ON `dishes_ingredients`.`ingredient_id` = `ingredients`.`id` WHERE `dishes`.`id` = {args}")
         ingredients = cursor.fetchall()
+        # print(ingredients)
         cursor.close()
         connect.close()
         self.render("dishinfo.html", dishes=dishes, ingredients=ingredients, timed=timed(), user=get_user_info(self))
+
 
 class testHandler(BaseHandler):
     @tornado.web.authenticated
@@ -93,9 +110,10 @@ class testHandler(BaseHandler):
         name = tornado.escape.xhtml_escape(self.current_user)
         self.write(f'Hi, {name}')
 
+
 class LoginHandler(BaseHandler):
-    def post(self): 
-        phone = re.sub('[^0-9]','', self.get_argument("phone"))
+    def post(self):
+        phone = re.sub('[^0-9]', '', self.get_argument("phone"))
         password = self.get_argument("password")
         connect = connections.getConnection()
         cursor = connect.cursor()
@@ -119,10 +137,12 @@ class LoginHandler(BaseHandler):
             self.clear_all_cookies(path="/")
             self.write("u need to be logined")
 
+
 class deauthHandler(BaseHandler):
     def get(self):
         self.clear_all_cookies(path="/")
         self.redirect("/")
+
 
 class viewUserHandler(BaseHandler):
     @tornado.web.authenticated
@@ -154,7 +174,8 @@ class viewUserHandler(BaseHandler):
             last_order.append(item['name'])
         cursor.close()
         connect.close()
-        self.render("userinfo.html", timed=timed(), user=get_user_info(self), userinfo=userinfo, orders=orders, last_order=last_order)
+        self.render("userinfo.html", timed=timed(), user=get_user_info(self), userinfo=userinfo, orders=orders,
+                    last_order=last_order)
 
     def post(self, args):
         new_name = self.get_argument('name')
@@ -166,10 +187,12 @@ class viewUserHandler(BaseHandler):
         connect.commit()
         self.redirect(f"/user/{args}")
 
+
 class changeUserInfo(BaseHandler):
     @tornado.web.authenticated
     def post(self):
         print('aza')
+
 
 class changeUserPreOrder(BaseHandler):
     @tornado.web.authenticated
@@ -178,8 +201,8 @@ class changeUserPreOrder(BaseHandler):
         elemid = dic['elemid']
         reason = dic['isadd']
         user_id = (get_user_info(self)['id'])
-        print(elemid)
-        print(reason)
+        # print(elemid)
+        # print(reason)
         connect = connections.getConnection()
         cursor = connect.cursor()
         cursor.execute(f"SELECT * FROM dishes WHERE id = {elemid}")
@@ -191,36 +214,167 @@ class changeUserPreOrder(BaseHandler):
     ON `users_orders`.`order_id` = `orders`.`id`\
   WHERE `users`.`id` = {user_id} ORDER BY order_id desc")
         last_order = cursor.fetchone()
-        print(last_order)
+        # print(last_order)
         if last_order['is_payed'] == "True":
             if reason:
-                print(12)
+                # print(12)
                 cursor.execute(f"INSERT INTO `diplom`.`orders` (`total_price`) VALUES ('{dish_price}')")
                 cursor.execute(f"SELECT * FROM orders order BY id desc")
                 order_id = cursor.fetchone()['id']
-                cursor.execute(f"INSERT INTO `diplom`.`users_orders` (`user_id`, `order_id`) VALUES ('{user_id}', '{order_id}')")
-                cursor.execute(f"INSERT INTO `diplom`.`dishes_order` (`dish_id`, `order_id`) VALUES ('{elemid}', '{order_id}')")
+                cursor.execute(
+                    f"INSERT INTO `diplom`.`users_orders` (`user_id`, `order_id`) VALUES ('{user_id}', '{order_id}')")
+                cursor.execute(
+                    f"INSERT INTO `diplom`.`dishes_order` (`dish_id`, `order_id`) VALUES ('{elemid}', '{order_id}')")
                 cursor.execute(f"UPDATE USERS SET last_order_id = {order_id} WHERE id = {user_id}")
                 connect.commit()
-                connect.close()     
+                connect.close()
         else:
             if reason:
-                print(1234)
+                # print(1234)
                 cursor.execute(f"SELECT * FROM orders order BY id desc")
                 order_id = cursor.fetchone()['id']
-                cursor.execute(f"UPDATE `diplom`.`orders` SET `total_price`= total_price + {dish_price} WHERE  `id`={order_id}")
-                cursor.execute(f"INSERT INTO `diplom`.`dishes_order` (`dish_id`, `order_id`) VALUES ('{elemid}', '{order_id}')")
+                cursor.execute(
+                    f"UPDATE `diplom`.`orders` SET `total_price`= total_price + {dish_price} WHERE  `id`={order_id}")
+                cursor.execute(
+                    f"INSERT INTO `diplom`.`dishes_order` (`dish_id`, `order_id`) VALUES ('{elemid}', '{order_id}')")
                 connect.commit()
                 connect.close()
             else:
-                print(12345)
+                # print(12345)
                 cursor.execute(f"SELECT * FROM orders order BY id desc")
                 order_id = cursor.fetchone()['id']
-                cursor.execute(f"UPDATE `diplom`.`orders` SET `total_price`= total_price - {dish_price} WHERE  `id`={order_id}")
-                cursor.execute(f"DELETE FROM `diplom`.`dishes_order` WHERE  `dish_id`={elemid} and `order_id`={order_id}")
+                cursor.execute(
+                    f"UPDATE `diplom`.`orders` SET `total_price`= total_price - {dish_price} WHERE  `id`={order_id}")
+                cursor.execute(
+                    f"DELETE FROM `diplom`.`dishes_order` WHERE  `dish_id`={elemid} and `order_id`={order_id}")
                 connect.commit()
                 connect.close()
-        print('pre order are changed')
+        # print('pre order are changed')
+
+
+class getDishesList(BaseHandler):
+    def post(self, *args):
+        dic = json.loads(self.request.body.decode('utf-8'))
+        connect = connections.getConnection()
+        cursor = connect.cursor()
+        cursor.execute(f"SELECT * FROM dishes \
+        INNER JOIN dish_types \
+        ON dishes.dish_type = dish_types.id where dish_types.id = {dic['new_val']}")
+        dishes = cursor.fetchall()
+        cursor.close()
+        connect.close()
+        # print(dishes)
+        dic = json.loads(self.request.body.decode('utf-8'))
+        # print(dic)
+        self.write(json.dumps({'status': 'ok', 'sent': dishes}))
+        self.finish()
+
+
+class dishTypeHandler(BaseHandler):
+    def get(self, args):
+        # print(args)
+        connect = connections.getConnection()
+        cursor = connect.cursor()
+        cursor.execute(f"SELECT * FROM dishes \
+INNER JOIN dish_types \
+ON dishes.dish_type = dish_types.id where dish_types.id = {args}")
+        dishes = cursor.fetchall()
+        cursor.execute(f"SELECT * FROM dish_types")
+        dish_types = cursor.fetchall()
+        try:
+            get_us = tornado.escape.xhtml_escape(self.current_user)
+        except Exception as e:
+            get_us = False
+        if get_us != False:
+            cursor.execute(f"select * from users where phone={get_us}")
+            userinfo = int(cursor.fetchone()['id'])
+            cursor.execute(f"SELECT dishes.id, dishes.name, dishes.price, dishes.description, dishes.img_src, dish_types.id, dishes.dish_type, dish_types.dish_type_name  FROM users   \
+            INNER JOIN users_orders \
+            ON users_orders.user_id = users.id \
+            JOIN dishes_order \
+            ON dishes_order.order_id = users_orders.order_id \
+            JOIN dishes \
+            ON dishes.id = dishes_order.dish_id \
+            JOIN dish_types \
+            ON dish_types.id = dishes.dish_type \
+            WHERE users.id = {userinfo} AND users.last_order_id = dishes_order.order_id ORDER BY dishes_order.order_id desc")
+            # cursor.execute(f"SELECT * FROM dish_types")
+            dishes_list = cursor.fetchall()
+            cursor.close()
+            connect.close()
+            self.render("index.html", dishes=dishes, timed=timed(), user=get_user_info(self), dishes_list=dishes_list,
+                        dish_types=dish_types)
+        else:
+            self.render("index.html", dishes=dishes, timed=timed(), user=get_user_info(self), dishes_list=None,
+                        dish_types=dish_types)
+
+
+class viewUserBasket(BaseHandler):
+    @tornado.web.authenticated
+    def get(self, args):
+        connect = connections.getConnection()
+        cursor = connect.cursor()
+        cursor.execute(f"select * from users where id={args}")
+        userinfo = cursor.fetchone()
+        cursor.execute(f"SELECT dishes.name, dishes.price, dishes.id, dishes_order.count, users_orders.order_id, dishes_order.id as d_o_id FROM `users`\
+  INNER JOIN `users_orders`\
+    ON `users_orders`.`user_id` = `users`.`id`\
+  LEFT JOIN `orders`\
+    ON `users_orders`.`order_id` = `orders`.`id`\
+    JOIN dishes_order\
+    ON dishes_order.order_id = users_orders.order_id\
+    JOIN dishes\
+    ON dishes.id = dishes_order.dish_id\
+  WHERE `users`.`id` = {args} AND users.last_order_id = orders.id;")
+        last_orders = cursor.fetchall()
+        last_order = []
+        for item in last_orders:
+            last_order.append({'dish_name': item['name'], 'dish_price': item['price'], 'dish_id': item['id'],
+                               'dish_count': item['count'], 'users_orders_id': item['order_id'], 'dishes_order_id': item['d_o_id']})
+        cursor.close()
+        connect.close()
+        self.render("userbasket.html", timed=timed(), user=get_user_info(self), userinfo=userinfo,
+                    last_order=last_order)
+
+
+class userChangeDishCount(BaseHandler):
+    @tornado.web.authenticated
+    def post(self, *args):
+        dic = json.loads(self.request.body.decode('utf-8'))
+        order_id = dic['order_id']
+        way = dic['way']
+        user_id = (get_user_info(self)['id'])
+        # print(elemid)
+        # print(reason)
+        connect = connections.getConnection()
+        cursor = connect.cursor()
+        if way == "up":
+            cursor.execute(f"UPDATE `diplom`.`dishes_order` SET `count`= `count` + 1 WHERE  `id`={order_id}")
+            connect.commit()
+        elif way == "down":
+            cursor.execute(f"UPDATE `diplom`.`dishes_order` SET `count`= `count` - 1 WHERE  `id`={order_id}")
+            connect.commit()
+        else:
+            print('SOME ERROR!')
+        cursor.close()
+        connect.close()
+
+
+class removeDishFromOrder(BaseHandler):
+    @tornado.web.authenticated
+    def post(self, args):
+        user_id = (get_user_info(self)['id'])
+        dish_cost = self.request.body.decode('utf8')[:-1]
+        connect = connections.getConnection()
+        cursor = connect.cursor()
+        cursor.execute(f"DELETE FROM `diplom`.`dishes_order` WHERE  `id`={args}")
+        connect.commit()
+        cursor.execute(f"SELECT * FROM `users` where id = {user_id}")
+        last_order_id = cursor.fetchone()['last_order_id']
+        cursor.execute(f"UPDATE `diplom`.`orders` SET `total_price`=`total_price` - {dish_cost} WHERE  `id`={last_order_id}")
+        connect.commit()
+        self.redirect("/basket/" + str(user_id))
+
 
 def make_app():
     return tornado.web.Application([
@@ -232,6 +386,11 @@ def make_app():
         (r"/user/([0-9]+)", viewUserHandler),
         (r"/user/changeUserInfo", changeUserInfo),
         (r"/changeUserPreOrder", changeUserPreOrder),
+        (r"/getDishesList", getDishesList),
+        (r"/dish_types/([0-9]+)", dishTypeHandler),
+        (r"/basket/([0-9]+)", viewUserBasket),
+        (r"/userChangeDishCount", userChangeDishCount),
+        (r"/removeDishFromOrder/([0-9]+)", removeDishFromOrder)
         # (r"/post/([0-9]+)", ViewPostHandler),
     ], **settings,
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
--- sqln.sql
+++ sqln.sql
@@ -27,20 +27,15 @@ CREATE TABLE IF NOT EXISTS `dishes` (
   PRIMARY KEY (`id`),
   KEY `FK_dishes_dish_types` (`dish_type`),
   CONSTRAINT `FK_dishes_dish_types` FOREIGN KEY (`dish_type`) REFERENCES `dish_types` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.dishes: ~9 rows (приблизительно)
+-- Дамп данных таблицы diplom.dishes: ~4 rows (приблизительно)
 /*!40000 ALTER TABLE `dishes` DISABLE KEYS */;
 INSERT IGNORE INTO `dishes` (`id`, `name`, `price`, `description`, `img_src`, `dish_type`) VALUES
-	(8, 'Burger1', 120, 'Очень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургер', 'dish1.png', 1),
-	(9, 'Burger2', 120, 'Очень вкусный бургер', 'dish2.png', 1),
-	(10, 'Burger3', 120, 'Очень вкусный бургер', 'dish3.png', 1),
-	(11, 'Burger4', 120, 'Очень вкусный бургер', 'dish1.png', 1),
-	(12, 'Burger5', 120, 'Очень вкусный бургер', 'dish1.png', 1),
-	(13, 'Burger6', 120, 'Очень вкусный бургер', 'dish1.png', 1),
-	(14, 'Burger7', 120, 'Очень вкусный бургер', 'dish1.png', 1),
-	(15, 'Burger8', 120, 'Очень вкусный бургер', 'dish1.png', 1),
-	(16, 'Burger9', 120, 'Очень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургерОчень вкусный бургер', 'dish1.png', 1);
+	(1, 'Сырный Джо', 220, 'Котлета из 100% говядины, покрытая плавленным сыром, сыром Пармезан и сыром Чеддер, со свежими помидорами, листовым салатом, все заправлено двумя соусами: Цезарь и Сырный.', 'dish_sirnii_djo.png', 1),
+	(2, 'Биг Кинг', 150, 'Два бифштекса из 100% говядины с фирменным соусом «Биг Кинг», ломтики сыра, листовой салат, хрустящие огурчики и свежий лук на мягкой булочке с кунжутом.', 'dish_big_king.png', 1),
+	(3, 'Воппер с сыром', 227, 'Легендарный бургер с бифштексом из 100% говядины на нежной булочке с кунжутом, заправленный сочными помидорами, двумя ломтиками сыра, листовым салатом, майонезом, хрустящими огурчиками и луком.', 'dish_vopper_s_sirom.png', 1),
+	(4, 'Салат Цезарь с креветками', 300, 'Легкий салат Цезарь с листьями салата айсберг, нежными королевскими креветками в легкой хрустящей панировке, сыром пармезан и чесночными сухариками под нежным соусом Цезарь.', 'dish_salad_cesar_s_krevetkami.png', 2);
 /*!40000 ALTER TABLE `dishes` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.dishes_ingredients
@@ -54,24 +49,32 @@ CREATE TABLE IF NOT EXISTS `dishes_ingredients` (
   KEY `ingredient_fk_1` (`ingredient_id`),
   CONSTRAINT `dish_fk_1` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE CASCADE,
   CONSTRAINT `ingredient_fk_1` FOREIGN KEY (`ingredient_id`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.dishes_ingredients: ~13 rows (приблизительно)
+-- Дамп данных таблицы diplom.dishes_ingredients: ~21 rows (приблизительно)
 /*!40000 ALTER TABLE `dishes_ingredients` DISABLE KEYS */;
 INSERT IGNORE INTO `dishes_ingredients` (`id`, `dish_id`, `ingredient_id`, `weight`) VALUES
-	(27, 8, 1, 100),
-	(28, 8, 2, 100),
-	(29, 8, 3, 100),
-	(31, 9, 3, 100),
-	(32, 9, 3, 100),
-	(33, 10, 3, 100),
-	(34, 10, 3, 100),
-	(35, 11, 3, 100),
-	(36, 12, 3, 100),
-	(37, 13, 3, 100),
-	(38, 14, 3, 100),
-	(39, 15, 3, 100),
-	(40, 16, 3, 100);
+	(1, 4, 1, 10),
+	(2, 4, 2, 10),
+	(3, 4, 3, 10),
+	(4, 4, 4, 10),
+	(5, 4, 5, 10),
+	(6, 4, 6, 10),
+	(7, 1, 7, 100),
+	(8, 1, 9, 10),
+	(9, 1, 10, 10),
+	(10, 1, 11, 10),
+	(11, 1, 12, 10),
+	(12, 2, 7, 100),
+	(13, 2, 9, 10),
+	(14, 2, 10, 10),
+	(15, 2, 11, 10),
+	(16, 2, 12, 10),
+	(17, 3, 7, 100),
+	(18, 3, 9, 10),
+	(19, 3, 10, 10),
+	(20, 3, 11, 10),
+	(21, 3, 12, 10);
 /*!40000 ALTER TABLE `dishes_ingredients` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.dishes_order
@@ -79,34 +82,21 @@ CREATE TABLE IF NOT EXISTS `dishes_order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dish_id` int(11) NOT NULL DEFAULT 0,
   `order_id` int(11) NOT NULL DEFAULT 0,
+  `count` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   KEY `dish_fk_2` (`dish_id`),
   KEY `order_fk_2` (`order_id`),
   CONSTRAINT `dish_fk_2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`),
   CONSTRAINT `order_fk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.dishes_order: ~3 rows (приблизительно)
+-- Дамп данных таблицы diplom.dishes_order: ~4 rows (приблизительно)
 /*!40000 ALTER TABLE `dishes_order` DISABLE KEYS */;
-INSERT IGNORE INTO `dishes_order` (`id`, `dish_id`, `order_id`) VALUES
-	(3, 16, 5),
-	(4, 15, 6),
-	(5, 10, 7),
-	(6, 10, 8),
-	(7, 9, 9),
-	(29, 15, 9),
-	(30, 8, 11),
-	(31, 9, 11),
-	(32, 10, 11),
-	(33, 11, 11),
-	(34, 12, 11),
-	(35, 13, 11),
-	(36, 14, 11),
-	(37, 15, 11),
-	(38, 16, 11),
-	(39, 8, 11),
-	(40, 9, 11),
-	(41, 10, 11);
+INSERT IGNORE INTO `dishes_order` (`id`, `dish_id`, `order_id`, `count`) VALUES
+	(64, 1, 1, 2),
+	(65, 4, 1, 2),
+	(66, 3, 1, 2),
+	(67, 2, 1, 2);
 /*!40000 ALTER TABLE `dishes_order` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.dish_types
@@ -114,14 +104,20 @@ CREATE TABLE IF NOT EXISTS `dish_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `dish_type_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.dish_types: ~3 rows (приблизительно)
+-- Дамп данных таблицы diplom.dish_types: ~9 rows (приблизительно)
 /*!40000 ALTER TABLE `dish_types` DISABLE KEYS */;
 INSERT IGNORE INTO `dish_types` (`id`, `dish_type_name`) VALUES
 	(1, 'Бургеры'),
-	(2, 'Что-то еще'),
-	(3, 'И еще что-то');
+	(2, 'Салаты'),
+	(3, 'Напитки'),
+	(4, 'Картошка'),
+	(5, 'Закуски'),
+	(6, 'Роллы'),
+	(7, 'Десерты'),
+	(8, 'Новинки'),
+	(9, 'Дополнительно');
 /*!40000 ALTER TABLE `dish_types` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.ingredients
@@ -131,16 +127,23 @@ CREATE TABLE IF NOT EXISTS `ingredients` (
   `description` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `calories` int(11) DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.ingredients: ~5 rows (приблизительно)
+-- Дамп данных таблицы diplom.ingredients: ~12 rows (приблизительно)
 /*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
 INSERT IGNORE INTO `ingredients` (`id`, `name`, `description`, `calories`) VALUES
-	(1, 'Кунжутная булочка', 'Смачная булочка с кунжутом', 20),
-	(2, 'Кусок мяса', 'Отбивная приготовленная на огне', 500),
-	(3, 'Салат', 'Капуста', 10),
-	(4, 'Ничего', 'Ничего', 0),
-	(5, 'Что-то', 'Что-то', 1);
+	(1, 'Листья салата Айсберг', 'Салат Айсберг Салат Айсберг – овощная культура, относящаяся к кочанным салатам, имеет светло-зелёные листья, завернутые в небольшие, не очень плотные кочаны, сочные и хрустящие на вкус.', 14),
+	(2, 'Королевские креветки', 'Королевские креветки – это не определенный вид ракообразных, а общее название больших по размеру креветок.', 87),
+	(3, 'Панировочные сухари', 'Панировочные сухари — сухарная крошка из пшеничного хлеба, используемая для обсыпки обжариваемых изделий из мяса, рыбы и овощей.', 395),
+	(4, 'Сыр пармезан', 'Пармеза́н — итальянский сорт твёрдого сыра долгого созревания.', 431),
+	(5, 'Чесночные сухарики', 'Сухарики с ароматом чеснока.', 239),
+	(6, 'Соус Цезарь', 'Соус с именем великого полководца.', 361),
+	(7, 'Котлета из говядины', 'Сочная котлета из 100% говядины.', 260),
+	(8, 'Плавленный сыр', NULL, 148),
+	(9, 'Сыр Чеддер', 'Чеддер — популярный английский сыр жирностью от 35 до 45%.', 402),
+	(10, 'Листовой салат', 'Лату́к посевной, или Сала́т латук — вид однолетних травянистых растений рода Латук семейства Астровые.', 15),
+	(11, 'Сырный соус', 'Всеми любимый сырный соус.', 356),
+	(12, 'Булочка для бургера', NULL, 272);
 /*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.orders
@@ -150,24 +153,12 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `date` datetime NOT NULL DEFAULT current_timestamp(),
   `is_payed` text COLLATE utf8mb4_unicode_ci DEFAULT 'False',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.orders: ~5 rows (приблизительно)
+-- Дамп данных таблицы diplom.orders: ~1 rows (приблизительно)
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
 INSERT IGNORE INTO `orders` (`id`, `total_price`, `date`, `is_payed`) VALUES
-	(1, 300, '2020-02-22 01:20:50', 'True'),
-	(2, 200, '2020-02-22 01:20:50', 'True'),
-	(3, 200, '2020-02-22 01:20:50', 'True'),
-	(4, 200, '2020-02-22 01:20:50', 'True'),
-	(5, 500, '2020-02-24 23:37:59', 'True'),
-	(6, 650, '2020-02-24 23:45:40', 'True'),
-	(7, 120, '2020-02-24 23:57:14', 'True'),
-	(8, 120, '2020-02-24 23:57:17', 'True'),
-	(9, 120, '2020-02-24 23:57:59', 'True'),
-	(10, 860, '2020-02-25 00:00:01', 'True'),
-	(11, 1440, '2020-02-25 00:19:49', 'True'),
-	(12, 0, '2020-02-25 00:39:54', 'True'),
-	(13, 0, '2020-02-25 00:53:32', 'False');
+	(1, 897, '2020-03-11 19:10:11', 'False');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.users
@@ -180,12 +171,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `last_order_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `last_order_id_fk_1` (`last_order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Дамп данных таблицы diplom.users: ~1 rows (приблизительно)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT IGNORE INTO `users` (`id`, `phone`, `password`, `name`, `image`, `last_order_id`) VALUES
-	(2, '79196413561', '123qwe', 'Egor', 'adm.jpg', 13);
+	(1, '79196413561', '123qwe', 'Egor', 'adm.jpg', 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Дамп структуры для таблица diplom.users_orders
@@ -198,22 +189,12 @@ CREATE TABLE IF NOT EXISTS `users_orders` (
   KEY `order_fk_1` (`order_id`),
   CONSTRAINT `order_fk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
   CONSTRAINT `user_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Дамп данных таблицы diplom.users_orders: ~3 rows (приблизительно)
+-- Дамп данных таблицы diplom.users_orders: ~1 rows (приблизительно)
 /*!40000 ALTER TABLE `users_orders` DISABLE KEYS */;
 INSERT IGNORE INTO `users_orders` (`id`, `user_id`, `order_id`) VALUES
-	(20, 2, 1),
-	(21, 2, 2),
-	(24, 2, 5),
-	(25, 2, 6),
-	(26, 2, 7),
-	(27, 2, 8),
-	(28, 2, 9),
-	(29, 2, 10),
-	(30, 2, 11),
-	(31, 2, 12),
-	(32, 2, 13);
+	(2, 1, 1);
 /*!40000 ALTER TABLE `users_orders` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
